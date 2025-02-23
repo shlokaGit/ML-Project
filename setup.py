58c1a5634c351f67c9a28a02ec36518b108b6944
shlokaGit <shlokabhattms@gmail.com>
2025-02-23 17:58:12 -0500

    logging exception

--- setup.py
+++ setup.py
@@ -13,11 +13,13 @@ def get_requirements(file_path:str)->List[str]:
 
     if HYPHEN_E_DOT in requirements:
         requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
 
 setup(
 name='ML-Project',
 version='0.0.1',
-author='Shloka',
+author='shlokaGit',
 author_email='shlokabhattms@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
